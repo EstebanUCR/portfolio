d4cc7c4bdaa1daa54941e59400a47e58ec0e4285
Esteban Iglesias Vargas <Esteban.Iglesias@ucr.ac.cr>
2025-11-05 18:40:05 -0600

    Adding header to the layout

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@styles/globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "@components/Header/header";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
         <main>{children}</main>
       </body>
     </html>
